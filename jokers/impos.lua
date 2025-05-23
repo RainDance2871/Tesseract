7ff64d88ab0b164d1d8d96b979ac2f6d094b8ec0
RainDance2871 <erik.nemmers@gmail.com>
2025-05-23 11:24:42 +0200

    Update impos.lua
    Fixed a bug for Impossibility where playing a 2 or 8 but not both caused Impossibility to trigger if you played the other in a later hand.

--- jokers/impos.lua
+++ jokers/impos.lua
@@ -1,6 +1,6 @@
 SMODS.Joker { --impos
   key = 'impos',
-  config = { extra = { Xmult = 3, twos = false, eights = false } },
+  config = { extra = { Xmult = 3 } },
   rarity = 2,
   atlas = 'T.Jokers',
   pos = { x = 3, y = 1 },
@@ -12,22 +12,24 @@ SMODS.Joker { --impos
   blueprint_compat = true,
   eternal_compat = true,
   calculate = function(self, card, context)
-    if context.individual and context.cardarea == G.play then
+    if context.joker_main then
+      local contains_two = false
+      local contains_eight = false
+      
       for i = 1, #context.scoring_hand do
-        if context.other_card:get_id() == 2 then
-          card.ability.extra.twos = true
-        elseif context.other_card:get_id() == 8 then
-          card.ability.extra.eights = true
+        if context.scoring_hand[i]:get_id() == 2 then
+          contains_two = true;
+        elseif context.scoring_hand[i]:get_id() == 8 then
+          contains_eight = true;
         end
       end
-    end
-    if context.joker_main and card.ability.extra.twos == true and card.ability.extra.eights == true then
-      card.ability.extra.twos = false
-      card.ability.extra.eights = false
-      return {
-        message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
-        Xmult_mod = card.ability.extra.Xmult
-      }
+      
+      if contains_two and contains_eight then
+        return {
+          message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
+          Xmult_mod = card.ability.extra.Xmult
+        }
+      end
     end
   end
 }

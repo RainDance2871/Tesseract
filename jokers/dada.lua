0a277a4c1e19ab72093a7f4063540b6e8a9a1e96
RainDance2871 <erik.nemmers@gmail.com>
2025-06-24 21:51:17 +0200

    Add files via upload

--- jokers/dada.lua
+++ jokers/dada.lua
@@ -1,28 +1,33 @@
-if not next(SMODS.find_mod('extracredit')) or tes.config.suppression_plushie ~= 2 then
-  SMODS.Joker { --dada
-    key = 'dada',
-    rarity = 1,
-    atlas = 'T.Jokers',
-    pos = { x = 2, y = 1 },
-    cost = 5,
-    perishable_compat = true,
-    blueprint_compat = true,
-    eternal_compat = true,
-    config = { extra = { chips_add = 20 } },
-    loc_vars = function(self, info_queue, card)
-      local total_cards = (G.jokers and #G.jokers.cards or 0)
-      return { vars = { card.ability.extra.chips_add, total_cards*card.ability.extra.chips_add } }
-    end,
-
-    calculate = function(self, card, context)
-      if context.joker_main then
-        local total_cards = #G.jokers.cards
-        return {
-          message = localize{type='variable', key='a_chips', vars={ total_cards * card.ability.extra.chips_add }},
-          chip_mod = total_cards * card.ability.extra.chips_add,
-          colour = G.C.CHIPS
-        }
-      end
+SMODS.Joker { --dada
+  key = 'dada',
+  rarity = 1,
+  atlas = 'T.Jokers',
+  pos = { x = 2, y = 1 },
+  cost = 5,
+  perishable_compat = true,
+  blueprint_compat = true,
+  eternal_compat = true,
+  in_pool = function(self, args)
+    if (next(SMODS.find_mod('extracredit')) and tes.config.suppression_plushie == 2) then
+      return false
     end
-  }
-end
+    return true
+  end,
+  no_collection = (next(SMODS.find_mod('extracredit')) and tes.config.suppression_plushie == 2) and true or nil,
+  config = { extra = { chips_add = 20 } },
+  loc_vars = function(self, info_queue, card)
+    local total_cards = (G.jokers and #G.jokers.cards or 0)
+    return { vars = { card.ability.extra.chips_add, total_cards*card.ability.extra.chips_add } }
+  end,
+
+  calculate = function(self, card, context)
+    if context.joker_main then
+      local total_cards = #G.jokers.cards
+      return {
+        message = localize{type='variable', key='a_chips', vars={ total_cards * card.ability.extra.chips_add }},
+        chip_mod = total_cards * card.ability.extra.chips_add,
+        colour = G.C.CHIPS
+      }
+    end
+  end
+}
--- jokers/mint.lua
+++ jokers/mint.lua
@@ -1,55 +1,60 @@
-if not next(SMODS.find_mod('plantain')) or tes.config.suppression_apple_pie ~= 2 then
-  SMODS.Joker { --mint
-    key = 'mint',
-    pools = { Food = true },
-    config = { extra = { money = 7, money_decrease = 1 } },
-    rarity = 2,
-    atlas = 'T.Jokers',
-    pos = { x = 1, y = 0 },
-    cost = 8,
-    eternal_compat = false,
-    blueprint_compat = false,
-    perishable_compat = true,
-    loc_vars = function(self, info_queue, card)
-      return { vars = { card.ability.extra.money, card.ability.extra.money_decrease } }
-    end,
+SMODS.Joker { --mint
+  key = 'mint',
+  pools = { Food = true },
+  config = { extra = { money = 7, money_decrease = 1 } },
+  rarity = 2,
+  atlas = 'T.Jokers',
+  pos = { x = 1, y = 0 },
+  cost = 8,
+  eternal_compat = false,
+  blueprint_compat = false,
+  perishable_compat = true,
+  in_pool = function(self, args)
+    if (next(SMODS.find_mod('plantain')) and tes.config.suppression_apple_pie == 2) then
+      return false
+    end
+    return true
+  end,
+  no_collection = (next(SMODS.find_mod('plantain')) and tes.config.suppression_apple_pie == 2) and true or nil,
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.money, card.ability.extra.money_decrease } }
+  end,
 
-    calc_dollar_bonus = function(self, card)
-      local bonus = card.ability.extra.money
-      if bonus > 0 then return bonus end
-    end,
+  calc_dollar_bonus = function(self, card)
+    local bonus = card.ability.extra.money
+    if bonus > 0 then return bonus end
+  end,
 
-    calculate = function(self, card, context)
-      if context.tesseract_mint_payout and not context.blueprint then
-        card.ability.extra.money = card.ability.extra.money - card.ability.extra.money_decrease
-        if card.ability.extra.money <= 0 then
-          G.E_MANAGER:add_event(Event({
-            func = function()
-              play_sound('tarot1')
-              card.T.r = -0.2
-              card:juice_up(0.3, 0.4)
-              card.states.drag.is = true
-              card.children.center.pinch.x = true
-              G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                func = function()
-                  G.jokers:remove_card(card)
-                  card:remove()
-                  card = nil
-                  return true
-                end
-              }))
-              return true
-            end
-          }))
-      
-          card_eval_status_text(card, 'jokers', nil, nil, nil, {message = localize('k_eaten_ex'), colour = G.C.RED})
-        else
-          card_eval_status_text(card, 'jokers', nil, nil, nil, {message = '-$1', colour = G.C.GOLD})
-        end
+  calculate = function(self, card, context)
+    if context.tesseract_mint_payout and not context.blueprint then
+      card.ability.extra.money = card.ability.extra.money - card.ability.extra.money_decrease
+      if card.ability.extra.money <= 0 then
+        G.E_MANAGER:add_event(Event({
+          func = function()
+            play_sound('tarot1')
+            card.T.r = -0.2
+            card:juice_up(0.3, 0.4)
+            card.states.drag.is = true
+            card.children.center.pinch.x = true
+            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+              func = function()
+                G.jokers:remove_card(card)
+                card:remove()
+                card = nil
+                return true
+              end
+            }))
+            return true
+          end
+        }))
+    
+        card_eval_status_text(card, 'jokers', nil, nil, nil, {message = localize('k_eaten_ex'), colour = G.C.RED})
+      else
+        card_eval_status_text(card, 'jokers', nil, nil, nil, {message = '-$1', colour = G.C.GOLD})
       end
     end
-  }
-end
+  end
+}
 
 --override of original function.
 local old_func_evaluate_round = G.FUNCS.evaluate_round
